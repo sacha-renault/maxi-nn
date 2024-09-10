722ae0d7609c387b89fac5c4fe134a74b441ed5a
sacha-renault <sacha35340@gmail.com>
2024-09-10 12:29:48 +0200

    feat: nn::math add a lot of function / activation

--- include/MaxiNn/Math.hpp
+++ include/MaxiNn/Math.hpp
@@ -68,4 +68,85 @@ namespace nn::math
         output->addChild(input);
         return output;
     }
+
+    template <typename T>
+    std::shared_ptr<tensor::Tensor<T>> exp(std::shared_ptr<tensor::Tensor<T>> input) {
+        auto exp_forward = [](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return input.array().exp();
+        };
+
+        auto exp_backward = [](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return input.array().exp();  // Same as forward for exp
+        };
+
+        auto output_val = exp_forward(input->getValues());
+        auto output = tensor::Tensor<T>::create(input->shape(), output_val, std::make_shared<Operation::ElementWiseOperation<T>>(exp_forward, exp_backward));
+        output->addChild(input);
+        return output;
+    }
+
+    template <typename T>
+    std::shared_ptr<tensor::Tensor<T>> log(std::shared_ptr<tensor::Tensor<T>> input) {
+        auto log_forward = [](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return input.array().log();
+        };
+
+        auto log_backward = [](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return input.array().inverse();  // 1 / x
+        };
+
+        auto output_val = log_forward(input->getValues());
+        auto output = tensor::Tensor<T>::create(input->shape(), output_val, std::make_shared<Operation::ElementWiseOperation<T>>(log_forward, log_backward));
+        output->addChild(input);
+        return output;
+    }
+
+    template <typename T>
+    std::shared_ptr<tensor::Tensor<T>> sqrt(std::shared_ptr<tensor::Tensor<T>> input) {
+        auto sqrt_forward = [](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return input.array().sqrt();
+        };
+
+        auto sqrt_backward = [](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return 0.5 * input.array().inverse().sqrt();  // 1 / (2 * sqrt(x))
+        };
+
+        auto output_val = sqrt_forward(input->getValues());
+        auto output = tensor::Tensor<T>::create(input->shape(), output_val, std::make_shared<Operation::ElementWiseOperation<T>>(sqrt_forward, sqrt_backward));
+        output->addChild(input);
+        return output;
+    }
+
+    template <typename T>
+    std::shared_ptr<tensor::Tensor<T>> inverse(std::shared_ptr<tensor::Tensor<T>> input) {
+        auto inverse_forward = [](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return input.array().inverse();
+        };
+
+        auto inverse_backward = [](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return -input.array().inverse().square();  // -1 / x^2
+        };
+
+        auto output_val = inverse_forward(input->getValues());
+        auto output = tensor::Tensor<T>::create(input->shape(), output_val, std::make_shared<Operation::ElementWiseOperation<T>>(inverse_forward, inverse_backward));
+        output->addChild(input);
+        return output;
+    }
+
+    template <typename T>
+    std::shared_ptr<tensor::Tensor<T>> clip(std::shared_ptr<tensor::Tensor<T>> input, T min_val, T max_val) {
+        auto clip_forward = [min_val, max_val](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return input.array().min(max_val).max(min_val);
+        };
+
+        auto clip_backward = [min_val, max_val](const Eigen::Matrix<T, Eigen::Dynamic, 1>& input) -> Eigen::Matrix<T, Eigen::Dynamic, 1> {
+            return ((input.array() > min_val) && (input.array() < max_val)).template cast<T>();
+        };
+
+        auto output_val = clip_forward(input->getValues());
+        auto output = tensor::Tensor<T>::create(input->shape(), output_val, std::make_shared<Operation::ElementWiseOperation<T>>(clip_forward, clip_backward));
+        output->addChild(input);
+        return output;
+    }
+
 } // namespace nn::math
