e733709f2bf7585955b85ac3a24c8cea68adb34d
sacha-renault <sacha35340@gmail.com>
2024-09-12 09:51:29 +0200

    feat: reduceSum and reduceMean

--- include/MaxiNn/TensorwiseMath.hpp
+++ include/MaxiNn/TensorwiseMath.hpp
@@ -9,98 +9,35 @@ namespace nn::math
         // First make a forward pass with the operation
         xt::xarray<T> valResult = nn::Operation::Dot<T>->forward({lt->getValues(), rt->getValues()});
 
-        // get the result and create a tensor with same shape -> set the result data inside 
+        // get the result and create a tensor with same shape -> set the result data inside
         auto result = tensor::Tensor<T>::create(valResult.shape(), valResult, nn::Operation::Dot<T>);
 
         result->addChild(lt);
         result->addChild(rt);
         return result;
     }
-//     template <typename T>
-//     std::shared_ptr<tensor::Tensor<T>> reduceMean(std::shared_ptr<tensor::Tensor<T>> input) {
-//         auto mean_forward = [](const xt::xarray<T>& input) -> xt::xarray<T> {
-//             // Compute the mean of all elements
-//             T mean_value = xt::mean(input)();
-//             return xt::xarray<T>({mean_value});
-//         };
 
-//         auto mean_backward = [](const xt::xarray<T>& input, const xt::xarray<T>& parent_values, const xt::xarray<T>& parent_grads) -> xt::xarray<T> {
-//             // The gradient should be evenly distributed across all elements of the input tensor
-//             return xt::full_like(input, parent_grads(0) / input.size());
-//         };
+    template <typename T>
+    std::shared_ptr<tensor::Tensor<T>> reduceSum(std::shared_ptr<tensor::Tensor<T>> input, xt::dynamic_shape<size_t> axis = xt::dynamic_shape<size_t>()) {
+        // First make a forward pass with the operation
+        xt::xarray<T> valResult = nn::Operation::ReduceSum<T>(axis)->forward({input->getValues()});
 
-//         auto output_val = mean_forward(input->getValues());
-//         auto output = tensor::Tensor<T>::create({1}, output_val, std::make_shared<nn::Operation::BatchWiseOperation<T>>(mean_forward, mean_backward));
-//         output->addChild(input);
-//         return output;
-//     }
+        // get the result and create a tensor with same shape -> set the result data inside
+        auto result = tensor::Tensor<T>::create(valResult.shape(), valResult, nn::Operation::ReduceSum<T>(axis));
 
-//     template <typename T>
-//     std::shared_ptr<tensor::Tensor<T>> reduceSum(std::shared_ptr<tensor::Tensor<T>> input) {
-//         auto sum_forward = [](const xt::xarray<T>& input) -> xt::xarray<T> {
-//             // Compute the sum of all elements
-//             T sum_value = xt::sum(input)();
-//             return xt::xarray<T>({sum_value});
-//         };
+        result->addChild(input);
+        return result;
+    }
 
-//         auto sum_backward = [](const xt::xarray<T>& input, const xt::xarray<T>& parent_values, const xt::xarray<T>& parent_grads) -> xt::xarray<T> {
-//             // The gradient should be the same value distributed across all elements of the input tensor
-//             return xt::full_like(input, parent_grads(0));
-//         };
+    template <typename T>
+    std::shared_ptr<tensor::Tensor<T>> reduceMean(std::shared_ptr<tensor::Tensor<T>> input, xt::dynamic_shape<size_t> axis = xt::dynamic_shape<size_t>()) {
+        // First make a forward pass with the operation
+        xt::xarray<T> valResult = nn::Operation::ReduceMean<T>(axis)->forward({input->getValues()});
 
-//         auto output_val = sum_forward(input->getValues());
-//         auto output = tensor::Tensor<T>::create({1}, output_val, std::make_shared<nn::Operation::BatchWiseOperation<T>>(sum_forward, sum_backward));
-//         output->addChild(input);
-//         return output;
-//     }
+        // get the result and create a tensor with same shape -> set the result data inside
+        auto result = tensor::Tensor<T>::create(valResult.shape(), valResult, nn::Operation::ReduceMean<T>(axis));
 
-//     template <typename T>
-//     std::shared_ptr<tensor::Tensor<T>> reduceSum(std::shared_ptr<tensor::Tensor<T>> input, const std::vector<int>& axis) {
-//         auto sum_forward = [axis](const xt::xarray<T>& input) -> xt::xarray<T> {
-//             // Compute the sum along the specified axes
-//             auto sum_value = xt::sum(input, axis);
-//             return sum_value;
-//         };
-
-//         auto sum_backward = [axis](const xt::xarray<T>& input, const xt::xarray<T>& parent_values, const xt::xarray<T>& parent_grads) -> xt::xarray<T> {
-//             // The gradient should be the same value distributed across all elements of the input tensor
-//             xt::xarray<T> grad = xt::full_like(input, parent_grads(0));
-//             return xt::broadcast(grad, input.shape());
-//         };
-
-//         auto output_val = sum_forward(input->getValues());
-
-//         // Convert the shape to std::vector<size_t>
-//         std::vector<size_t> shape(output_val.shape().begin(), output_val.shape().end());
-
-//         // Assuming Tensor::create expects a shape and values
-//         auto output = tensor::Tensor<T>::create(shape, output_val,
-//             std::make_shared<nn::Operation::BatchWiseOperation<T>>(sum_forward, sum_backward));
-//         output->addChild(input);
-//         return output;
-//     }
-
-//     template <typename T>
-//     std::shared_ptr<tensor::Tensor<T>> dot(std::shared_ptr<tensor::Tensor<T>> a, std::shared_ptr<tensor::Tensor<T>> b) {
-//         auto sum_forward = [](const xt::xarray<T>& input) -> xt::xarray<T> {
-//             // TODO
-//         };
-
-//         auto sum_backward = [axis](const xt::xarray<T>& input, const xt::xarray<T>& parent_values, const xt::xarray<T>& parent_grads) -> xt::xarray<T> {
-//             // TODO
-//         };
-
-//         auto output_val = sum_forward(input->getValues());
-
-//         // Convert the shape to std::vector<size_t>
-//         std::vector<size_t> shape(output_val.shape().begin(), output_val.shape().end());
-
-//         // Assuming Tensor::create expects a shape and values
-//         auto output = tensor::Tensor<T>::create(shape, output_val,
-//             std::make_shared<nn::Operation::BatchWiseOperation<T>>(sum_forward, sum_backward));
-            
-//         output->addChild(a);
-//         output->addChild(b);
-//         return output;
-//     }
+        result->addChild(input);
+        return result;
+    }
 } // namespace nn::math
--- src/InternalOperation/ElementwiseOperation.hpp
+++ src/InternalOperation/ElementwiseOperation.hpp
@@ -22,7 +22,7 @@ namespace nn::Operation
             // Gradient of Tanh(x) is (1 - Tanh(x)^2)
             auto tanh_val = output_vals;
             auto grad_wrt_input = output_grads * (1 - xt::square(tanh_val));
-            
+
             return {grad_wrt_input};
         }
     );
@@ -184,45 +184,6 @@ namespace nn::Operation
                     output_grads
                 );
                 return {grad_wrt_input};
-            }
-        );
-    }
-
-    template <typename T>
-    std::shared_ptr<IOperation<T>> reduceMean(xt::dynamic_shape<size_t> axis = xt::dynamic_shape<size_t>()) {
-        return std::make_shared<IOperation<T>>(
-            [axis](const std::vector<std::reference_wrapper<const xt::xarray<T>>>& children_values) -> xt::xarray<T> {
-                if (children_values.size() != 1) {
-                    throw std::runtime_error("ReduceMean must have exactly one child");
-                }
-                const auto& child = children_values[0].get();
-                
-                if (axis.empty()) {
-                    // If no axis is provided, reduce over all dimensions
-                    return xt::mean(child);
-                } else {
-                    // Reduce along specified axis
-                    return xt::mean(child, axis);
-                }
-            },
-            [axis](const std::vector<std::reference_wrapper<const xt::xarray<T>>>& children_values,
-                       const xt::xarray<T>& output_vals,
-                       const xt::xarray<T>& output_grads) -> std::vector<xt::xarray<T>> {
-                if (children_values.size() != 1) {
-                    throw std::runtime_error("ReduceMean must have exactly one child");
-                }
-                const auto& child = children_values[0].get();
-
-                // Calculate the gradient with respect to the input tensor
-                std::vector<xt::xarray<T>> grad_wrt_input(1);
-                if (axis.empty()) {
-                    // If no axis is provided, replicate the gradient to match the input shape
-                    grad_wrt_input[0] = xt::broadcast(output_grads, child.shape()) / child.size();
-                } else {
-                    // Expand gradients to the shape of the original input
-                    grad_wrt_input[0] = xt::broadcast(output_grads, child.shape()) / xt::prod(child.shape(axis));
-                }
-                return grad_wrt_input;
             }
         );
     }
